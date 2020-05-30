ad68a3d935e0d8972df53c03782d6b6bf7e8aee9
PopovicV <vladimirpopovic@live.com>
2020-05-30 03:36:29 +0200

    Vehicle model

--- back/vehicle/src/main/java/com/example/vehicle/repository/VehicleRepository.java
+++ back/vehicle/src/main/java/com/example/vehicle/repository/VehicleRepository.java
@@ -1,7 +1,39 @@
 package com.example.vehicle.repository;
 
+import com.example.vehicle.model.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
-public class VehicleRepository {
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+
+    List<Vehicle> findAll();
+
+    Vehicle findOneById(Long Id);
+
+    Vehicle save(Vehicle vehicle);
+
+    void removeById(Long Id);
+
+    List<Vehicle> findByCollisionProtection(boolean collisionProtection);
+
+    List<Vehicle> findByChildrenSeats(int childrenSeats);
+
+    List<Vehicle> findByStartDate(Date startDate);
+
+    List<Vehicle> findByEndDate(Date endDate);
+
+    List<Vehicle> findByVehicleFuelTypeId(Long Id);
+
+    List<Vehicle> findByVehicleMakeId(Long Id);
+
+    List<Vehicle> findByVehicleModelId(Long Id);
+
+    List<Vehicle> findByVehicleStypeId(Long Id);
+
+    List<Vehicle> findByVehicleTransmissionId(Long Id);
 }
